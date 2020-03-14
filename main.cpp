add0f44edca2c3db5d256656b1b1e0cef27ac369
Svyat2000 <61850752+Svyat2000@users.noreply.github.com>
2020-03-14 21:51:59 +0300

    Задание 9

--- main.cpp
+++ main.cpp
@@ -7,24 +7,17 @@ using namespace std;
 int main(){
     ofstream file;
     file.open("c:\\Qt\\Projects\\1\\example4.txt");
-    double x,y;
-    cout<<"Enter x: ";
-    cin>>x;
-    if(x>2){
-      y=2*x-15;
-      cout<<y;
-      file<<y;
-    }
-    if(x==2.0){
-        y=5;
-        cout<<y;
-        file<<y;
-    }
-    if(x<2){
-        y=3*fabs(x-1)-8;
-        cout<<y;
-        file<<y;
-    }
+   double d,stsharov;
+   int kolvosharov;
+   cout<<"Vvedite vnutrennii diametr vnutrennego shara(sm),  tolsh'inu stenok sharov(mm), kolichestvo sharov: "<<endl;
+   cin>>d>>stsharov>>kolvosharov;
+   double V=(4/3*3.14*pow((d+kolvosharov*stsharov/10),3))/1000;
+   cout<<V;
+   file<<V;
+
+
+
+   file.close();
 
     return 0;
 }
