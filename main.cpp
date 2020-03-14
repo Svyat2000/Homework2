b55aa8a6bbf934363679360c7695dbeb73441a9b
Svyat2000 <61850752+Svyat2000@users.noreply.github.com>
2020-03-14 21:00:02 +0300

    Задание4(E)

--- main.cpp
+++ main.cpp
@@ -6,25 +6,28 @@ using namespace std;
 
 int main(){
     ofstream file;
-    file.open("c:\\Qt\\Projects\\1\\example3.txt");
-    file<<"\tx\t\ty"<<endl;
-    double shag,npredel,vpredel;
-    cout<<"Enter (npredel and vpredel): ";
-    cin>>npredel>>vpredel;
-    cout<<"Enter shag: ";
-    cin>>shag;
-    cout<<"\tx\t\ty"<<endl;
-    cout.precision(5);
-    for (double x=npredel;x<=vpredel;x+=shag) {
-        double y=sqrt(5*x+4*(pow(x,3)))+4*x+3/sin(9*x+71);
-        cout<<"\t"<<x<<"\t\t"<<y<<endl;
-        file<<"\t"<<x<<"\t\t"<<y<<endl;
+    file.open("c:\\Qt\\Projects\\1\\example4.txt");
+    double x,y;
+    cout<<"Enter x: ";
+    cin>>x;
+    if(x>2){
+      y=2*x-15;
+      cout<<y;
+      file<<y;
     }
-    file.close();
+    if(x==2.0){
+        y=5;
+        cout<<y;
+        file<<y;
+    }
+    if(x<2){
+        y=3*fabs(x-1)-8;
+        cout<<y;
+        file<<y;
+    }
 
     return 0;
 }
-
 
 
 
