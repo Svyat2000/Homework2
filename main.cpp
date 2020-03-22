38940478f6c5a9513894b851bf32002cb332b3f0
Svyat2000 <61850752+Svyat2000@users.noreply.github.com>
2020-03-22 16:48:47 +0300

    Задание3(i)
    Исправил суть программы, теперь правильно

--- main.cpp
+++ main.cpp
@@ -4,28 +4,40 @@
 #include <math.h>
 using namespace std;
 int fact (int ch);
-
 int main(){
     ofstream file;
     file.open("c:\\Qt\\Projects\\1\\example4.txt");
-     cout<<"Enter x: "<<endl;
-     double x;
-     cin>>x;
-     double y=1-(pow(x,2)/fact(2))+(pow(x,4)/fact(4));
-     cout<<y<<endl;
-     file<<y;
-     file.close();
+    double x,y=0;
+    int ch,i=1;
+    cout<< "Enter x: ";
+    cin>>x;
+    cout<< "Enter ch: ";
+    cin>>ch;
+    double sum=1;
+    for (i=1;i<=ch;i++){
+        if (i%2==1){
+           y=pow(x,2*i)/fact(i*2);
+           sum=sum-y;
+           }
+        else {
+             y=pow(x,2*i)/fact(i*2);
+             sum=sum+y;
+             }
+         }
+    cout<<sum<<endl;
+    file<<y;
+    file.close();
 
     return 0;
 }
-int fact(int ch){
 
-    if(ch < 0)
+int fact(int n){
+    if(n < 0)
             return 0;
-        if (ch == 0)
+    if (n == 0)
             return 1;
-        else
-            return ch * fact(ch - 1);
+    else
+            return n * fact(n - 1);
 }
 
 
